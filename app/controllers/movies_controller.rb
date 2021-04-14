b61453d7ca67d627a077ac14a86bad270b2be92b
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-14 21:07:52 +0000

    after some errors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new.html.erb"
+    
   end
 
   def index
@@ -15,8 +15,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index.html.erb"
+      format.html 
     end
   end
 
@@ -27,7 +26,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show.html.erb" 
+    
   end
 
   def create
@@ -37,9 +36,9 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to(movies_url, notice: "Movie created successfully." 
+      redirect_to movies_url, notice: "Movie created successfully." 
     else
-      render template: "movies/new.html.erb"
+      render "new"
     end
   end
 
@@ -50,7 +49,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/edit.html.erb" 
+
   end
 
   def update
@@ -62,9 +61,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to(movie_url(the_movie.id),  notice: "Movie updated successfully."
+      redirect_to movie_url(the_movie.id),  notice: "Movie updated successfully."
     else
-      redirect_to(movie_url(the_movie.id),  alert:  "Movie failed to update successfully." 
+      redirect_to movie_url(the_movie.id),  alert:  "Movie failed to update successfully." 
     end
   end
 
@@ -74,6 +73,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to(movies_url, notice: "Movie deleted successfully."
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
